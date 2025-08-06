80f17cf0dce0f7accd692565af499306e47c55ee
Yusuf İşeri <166063804+bagadarkhan@users.noreply.github.com>
2025-08-06 13:48:38 +0300

    house_prediction_model(2.0)
    kod okuması sırasında yorum satırlarında açıklamalar yazıyor.Modelin bu reposunda istatiksel analiz ve hata hesaplamalrı gibi geliştirmeler yaptım.

--- evfiyattahminin.py
+++ evfiyattahminin.py
@@ -1,40 +1,51 @@
-import matplotlib.pyplot
+import matplotlib.pyplot as plt
 import pandas
-import numpy 
+import numpy
+import statsmodels.api as sm
+#okuduk
+df=pandas.read_csv("ev_verileri.csv")
+#işlem yapabilmek için parçaladık
+X=df[["Alan","OdaSayısı","BinaYaşı"]]### burada kat değişkeninin önemsiz oldugunu kesfettik
+Y=df[["Fiyat"]]
 
-data=pandas.read_csv("ev_verileri.csv")
+#istatistiksel analiz aşaması hangi değişkenler anlamlı???
+X=sm.add_constant(X)
 
-print(data.head())
+model=sm.OLS(Y,X).fit()
 
-x=data[["Alan"]]
-y=data[["Fiyat"]] 
-
-
+print(model.summary())
 
 from sklearn.model_selection import train_test_split
-
-x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.33,random_state=0)
+#modeli eğittik
+X_train,X_test,Y_train,Y_test=train_test_split(X, Y,test_size=0.2,random_state=42)
 
 from sklearn.linear_model import LinearRegression
-
+#modeli inşaa ettik
 model=LinearRegression()
+model.fit(X_train, Y_train)
+#tahminler yapar hale geldi
+Y_pred=model.predict(X_test)
 
-model.fit(x_train,y_train)
+from sklearn.metrics import mean_squared_error,mean_absolute_error,r2_score
 
-tahmin=model.predict(x_test)
-print(tahmin)
+#hata payları
+mse=mean_squared_error(Y_test,Y_pred)
+mae=mean_absolute_error(Y_test,Y_pred)#ortalama hata payı
+r2=r2_score(Y_test,Y_pred)#uyumu ölçer
 
+#grafiği çizdik
+plt.figure(figsize=(8,6))
+plt.scatter(Y_test, Y_pred, color="blue", label="Tahminler")
+plt.plot([Y_test.min(), Y_test.max()], [Y_test.min(), Y_test.max()], 'r--', label="Mükemmel Tahmin")
+plt.xlabel("Gerçek Fiyatlar (₺)")
+plt.ylabel("Tahmin Edilen Fiyatlar (₺)")
+plt.title("Gerçek ve Tahmin Fiyat Karşılaştırması")
+plt.legend()
+plt.show()
 
-matplotlib.pyplot.scatter(x_test,y_test,color='blue',label="Gerçek değerler")
-matplotlib.pyplot.plot(x_test,tahmin,color="Green",label="Tahminler")
-matplotlib.pyplot.xlabel("Alan")
-matplotlib.pyplot.ylabel("fiyat")
-
-metrekare=float(input("Evin Metrekaresini giriniz: "))
-tahmin_fiyat=model.predict(([[metrekare]]))
-
-print(f"{metrekare} m2 evin tahmini fiyatı: {tahmin_fiyat[0][0]:,.2f}TL")
-
+#Modeli Yorumlama#
+print("Katsayılar:",model.coef_)
+print("Sabit katsayılar:",model.intercept_)
 
 
 
